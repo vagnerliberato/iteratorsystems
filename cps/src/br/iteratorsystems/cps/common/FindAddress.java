b8e3dd99dc1f2df06ccf9aea98e8f000bffa06a1
andresmafra <andresmafra@61f7c19e-d3f1-6563-da10-9b4bf67539cb>
2010-04-23 02:43:11 +0000

    Adicionando uma imagem para testar

--- cps/src/br/iteratorsystems/cps/common/FindAddress.java
+++ cps/src/br/iteratorsystems/cps/common/FindAddress.java
@@ -51,7 +51,7 @@ public class FindAddress {
 			connection.setDoInput(true);
 			connection.setDoOutput(false);
 
-			this.createProxy(connection);
+//			this.createProxy(connection);
 
 			connection.connect();
 		} catch (Exception e) {
