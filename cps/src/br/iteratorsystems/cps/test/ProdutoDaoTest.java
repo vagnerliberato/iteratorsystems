b52fdfcecc7661713f8a575a6f90fce1b5cdcf01
domingues.santiago <domingues.santiago@61f7c19e-d3f1-6563-da10-9b4bf67539cb>
2010-07-14 22:58:25 +0000

    Nova classe produtoDaoTest

--- cps/src/br/iteratorsystems/cps/test/ProdutoDaoTest.java
+++ cps/src/br/iteratorsystems/cps/test/ProdutoDaoTest.java
@@ -1,34 +1,53 @@
 package br.iteratorsystems.cps.test;
+import static org.junit.Assert.*;
 
-import static org.junit.Assert.assertEquals;
+import java.util.HashSet;
+import java.util.Set;
 
 import org.hibernate.Session;
 import org.junit.Test;
 
 import br.iteratorsystems.cps.config.HibernateConfig;
-import br.iteratorsystems.cps.dao.ProdutoGeralDao;
-import br.iteratorsystems.cps.entities.PRODUTOGERAL;
+import br.iteratorsystems.cps.dao.ProdutoDao;
+import br.iteratorsystems.cps.entities.PRECO;
+import br.iteratorsystems.cps.entities.PRECOID;
+import br.iteratorsystems.cps.entities.PRODUTO;
+import br.iteratorsystems.cps.entities.PRODUTOID;
 import br.iteratorsystems.cps.exceptions.CpsDaoException;
 
 public class ProdutoDaoTest {
 
 	@Test
-	public void deveRetornaOMesmoProdutoSalvo() throws CpsDaoException{
-		
-		PRODUTOGERAL produtogeral = new PRODUTOGERAL();
-		produtogeral.setCodigoBarras("000011112222");
-		produtogeral.setDescricao("Bolha");
-		produtogeral.setEmbalagem("5");
-		produtogeral.setUnidadeMedida("KG");
+	public void deveRetornarOMesmoProdutoSalvo() throws CpsDaoException{
 		
 		Session session = HibernateConfig.getSession();
-		ProdutoGeralDao produtoGeralDao = new ProdutoGeralDao(PRODUTOGERAL.class, session);
-		produtoGeralDao.salvar(produtogeral);
+		ProdutoDao produtoDao = new ProdutoDao(PRODUTO.class, session);
 		
-		PRODUTOGERAL produto = produtoGeralDao.obter("000011112222");
+		PRODUTO produto = new PRODUTO();
+		produto.setDescricao("CPS");
+		produto.setEmbalagem("10");
+		PRODUTOID produtoId = new PRODUTOID("111122223333", 3, 5);
+		produto.setId(produtoId);
+		Set<PRECO> precos = new HashSet<PRECO>();
+		PRECOID precoId = new PRECOID(5, 3, "111122223333");
+		String precoVarejo1 = "2.55";
+		PRECO preco1= new PRECO(precoId, produto, precoVarejo1);
+		PRECOID precoId2 = new PRECOID(5, 3, "111122224444");
+		String precoVarejo2 = "3.55";
+		PRECO preco2= new PRECO(precoId2, produto, precoVarejo2);
+		precos.add(preco1);
+		precos.add(preco2);
+		produto.setPrecos(precos);
 		
-		assertEquals("Bolha", produto.getDescricao());
-		assertEquals("5", produto.getEmbalagem());
-		assertEquals("KG", produto.getUnidadeMedida());
+		produtoDao.salvar(produto);
+		
+		PRODUTO produtoTest = produtoDao.obter(produtoId);
+		
+		assertEquals("CPS", produtoTest.getDescricao());
+		assertEquals(2, produtoTest.getPrecos().size());
+		assertSame(produto, produtoTest);
+		assertFalse(produtoTest.getPrecos().isEmpty());
+		assertEquals(produtoId, produtoTest.getPrecos().iterator().next().getProduto().getId());
 	}
+	
 }
