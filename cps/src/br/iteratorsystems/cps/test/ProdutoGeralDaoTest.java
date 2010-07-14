a3e8a45284980b32a986069b50c12e944ca7da10
domingues.santiago <domingues.santiago@61f7c19e-d3f1-6563-da10-9b4bf67539cb>
2010-07-14 22:57:43 +0000

    Fazendo melhorias na classe ProdutoGeralDaoTest

---
+++ cps/src/br/iteratorsystems/cps/test/ProdutoGeralDaoTest.java
@@ -0,0 +1,34 @@
+package br.iteratorsystems.cps.test;
+
+import static org.junit.Assert.assertEquals;
+
+import org.hibernate.Session;
+import org.junit.Test;
+
+import br.iteratorsystems.cps.config.HibernateConfig;
+import br.iteratorsystems.cps.dao.ProdutoGeralDao;
+import br.iteratorsystems.cps.entities.PRODUTOGERAL;
+import br.iteratorsystems.cps.exceptions.CpsDaoException;
+
+public class ProdutoGeralDaoTest {
+
+	@Test
+	public void deveRetornaOMesmoProdutoGeralSalvo() throws CpsDaoException{
+		
+		PRODUTOGERAL produtogeral = new PRODUTOGERAL();
+		produtogeral.setCodigoBarras("000011112222");
+		produtogeral.setDescricao("Bolha");
+		produtogeral.setEmbalagem("5");
+		produtogeral.setUnidadeMedida("KG");
+		
+		Session session = HibernateConfig.getSession();
+		ProdutoGeralDao produtoGeralDao = new ProdutoGeralDao(PRODUTOGERAL.class, session);
+		produtoGeralDao.salvar(produtogeral);
+		
+		PRODUTOGERAL produto = produtoGeralDao.obter("000011112222");
+		
+		assertEquals("Bolha", produto.getDescricao());
+		assertEquals("5", produto.getEmbalagem());
+		assertEquals("KG", produto.getUnidadeMedida());
+	}
+}
