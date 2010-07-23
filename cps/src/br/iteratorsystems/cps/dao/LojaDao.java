a2c9564a6c5741487a474344106a72dff8b4d9db
domingues.santiago <domingues.santiago@61f7c19e-d3f1-6563-da10-9b4bf67539cb>
2010-07-23 00:05:28 +0000

    Novo metodos de busca por nome

--- cps/src/br/iteratorsystems/cps/dao/LojaDao.java
+++ cps/src/br/iteratorsystems/cps/dao/LojaDao.java
@@ -1,14 +1,24 @@
 package br.iteratorsystems.cps.dao;
 
+import java.util.List;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 import br.iteratorsystems.cps.entities.LOJA;
 import br.iteratorsystems.cps.entities.LOJAID;
+import br.iteratorsystems.cps.exceptions.CpsDaoException;
 
 public class LojaDao extends DaoGeneric<LOJA, LOJAID> {
 
 	public LojaDao(Class<LOJA> persistentClass, Session session) {
 		super(persistentClass, session);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<LOJA> obterLojaPorNomeFantasia(String nomefantasia) throws HibernateException, CpsDaoException {
+		return getSession().createCriteria(LOJA.class).add(Restrictions.eq("nomefantasia", nomefantasia)).list();
 	}
 
 }
--- cps/src/br/iteratorsystems/cps/dao/RedeDao.java
+++ cps/src/br/iteratorsystems/cps/dao/RedeDao.java
@@ -1,13 +1,23 @@
 package br.iteratorsystems.cps.dao;
 
+import java.util.List;
+
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
 
 import br.iteratorsystems.cps.entities.REDE;
+import br.iteratorsystems.cps.exceptions.CpsDaoException;
 
 public class RedeDao extends DaoGeneric<REDE, Integer> {
 
 	public RedeDao(Class<REDE> persistentClass, Session session) {
 		super(persistentClass, session);
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<REDE> obterPorNome(String nomeRede) throws HibernateException, CpsDaoException {
+		return getSession().createCriteria(REDE.class).add(Restrictions.eq("nome", nomeRede)).list();
 	}
 
 }
