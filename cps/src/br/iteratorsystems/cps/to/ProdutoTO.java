f4e9d55d2b26e281d2f0ab91c0ec8a42ee5e32dd
andresmafra <andresmafra@61f7c19e-d3f1-6563-da10-9b4bf67539cb>
2010-07-24 13:22:34 +0000

    Produto TO alterado (imagem)

--- cps/src/br/iteratorsystems/cps/to/ProdutoTO.java
+++ cps/src/br/iteratorsystems/cps/to/ProdutoTO.java
@@ -1,5 +1,7 @@
 package br.iteratorsystems.cps.to;
 
+import java.io.Serializable;
+
 import br.iteratorsystems.cps.entities.PRODUTOGERAL;
 
 /**
@@ -7,19 +9,30 @@ import br.iteratorsystems.cps.entities.PRODUTOGERAL;
  * @author André
  *
  */
-public class ProdutoTO {
+public class ProdutoTO implements Serializable{
 
-	private PRODUTOGERAL produtoGeral;
-	private Integer quantidadeSelecionada;
+	private static final long serialVersionUID = 7446849227982421202L;
 	
+	private PRODUTOGERAL produtoGeral;
+	private Boolean possuiImagem;
+	private Integer quantidadeSelecionada;
+	private String imagem;
 	
 	/**
+	 * Construtor mínimo
 	 * @param produtoGeral
 	 * @param quantidadeSelecionada
 	 */
 	public ProdutoTO(PRODUTOGERAL produtoGeral, Integer quantidadeSelecionada) {
 		this.produtoGeral = produtoGeral;
 		this.quantidadeSelecionada = quantidadeSelecionada;
+	}
+	
+	/**
+	 * Construtor padrão
+	 */
+	public ProdutoTO() {
+		super();
 	}
 	
 	/**
@@ -74,5 +87,44 @@ public class ProdutoTO {
 			equals = false;
 		}
 		return equals;
+	}
+
+	/**
+	 * @param imagem the imagem to set
+	 */
+	public void setImagem(String imagem) {
+		this.imagem = imagem;
+	}
+
+	/**
+	 * @return the imagem
+	 */
+	public String getImagem() {
+		return imagem;
+	}
+
+	/**
+	 * @param possuiImagem the possuiImagem to set
+	 */
+	public void setPossuiImagem(Boolean possuiImagem) {
+		this.possuiImagem = possuiImagem;
+	}
+
+	/**
+	 * @return the possuiImagem
+	 */
+	public Boolean getPossuiImagem() {
+		return possuiImagem;
+	}
+	
+	/**
+	 * to String
+	 * @return Variaveis da classe em string
+	 */
+	@Override
+	public String toString() {
+		return "ProdutoTO [imagem=" + imagem + ", possuiImagem=" + possuiImagem
+				+ ", quantidadeSelecionada=" + quantidadeSelecionada + 
+				"codigoBarra="+produtoGeral.getCodigoBarras().trim()+"]";
 	}
 }
