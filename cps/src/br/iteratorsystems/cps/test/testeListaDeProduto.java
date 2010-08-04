3ced429c1f5da7bc34a422146855a3ad7c222195
domingues.santiago <domingues.santiago@61f7c19e-d3f1-6563-da10-9b4bf67539cb>
2010-08-04 22:32:16 +0000

    Criando teste da lista de produto

---
+++ cps/src/br/iteratorsystems/cps/test/testeListaDeProduto.java
@@ -0,0 +1,38 @@
+package br.iteratorsystems.cps.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import br.iteratorsystems.cps.beans.ListaDeProdutoBean;
+import br.iteratorsystems.cps.entities.Tabelas_ListaProdutoItem;
+import br.iteratorsystems.cps.entities.Tabelas_ProdutoGeral;
+import br.iteratorsystems.cps.exceptions.CpsGeneralExceptions;
+import br.iteratorsystems.cps.to.ProdutoTO;
+
+public class testeListaDeProduto {
+
+	public static void main(String[] args) throws CpsGeneralExceptions {
+		
+		Set<ProdutoTO> listaProdutoTO = new HashSet<ProdutoTO>();
+		ProdutoTO produtoTO = new ProdutoTO(popular(),12);
+		listaProdutoTO.add(produtoTO); 
+		ListaDeProdutoBean bean = new ListaDeProdutoBean();
+		
+		bean.setListaComprasUsuario(listaProdutoTO);
+		bean.incluirListaDeProdutos();
+	}
+	
+	private static Tabelas_ProdutoGeral popular() {
+		Tabelas_ProdutoGeral prod  = new Tabelas_ProdutoGeral();
+		Set<Tabelas_ListaProdutoItem> lista = new HashSet<Tabelas_ListaProdutoItem>();
+		prod.setCodigoBarras("101010101");
+		prod.setDescricao("teste");
+		prod.setEmbalagem("sei la");
+		prod.setImagem('N');
+		prod.setUnidadeMedida("KG");
+		prod.setStatus('A');
+		lista.add(new Tabelas_ListaProdutoItem(null, null, prod, 1));
+		prod.setListaProdutoItems(lista);
+		return prod;
+	}
+}
