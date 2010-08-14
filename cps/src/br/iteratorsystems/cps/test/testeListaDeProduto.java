aa9f01b836648a548edb919ee6b6ee004d37514d
domingues.santiago <domingues.santiago@61f7c19e-d3f1-6563-da10-9b4bf67539cb>
2010-08-14 15:32:44 +0000

    Adicionando metodos nos teste da lista produto

--- cps/src/br/iteratorsystems/cps/test/testeListaDeProduto.java
+++ cps/src/br/iteratorsystems/cps/test/testeListaDeProduto.java
@@ -3,29 +3,51 @@ package br.iteratorsystems.cps.test;
 import java.util.HashSet;
 import java.util.Set;
 
+import org.hibernate.LockMode;
+import org.hibernate.Session;
+
 import br.iteratorsystems.cps.beans.ListaDeProdutoBean;
+import br.iteratorsystems.cps.config.HibernateConfig;
+import br.iteratorsystems.cps.dao.ListaProdutoDao;
+import br.iteratorsystems.cps.entities.Tabelas_ListaProduto;
 import br.iteratorsystems.cps.entities.Tabelas_ListaProdutoItem;
 import br.iteratorsystems.cps.entities.Tabelas_ProdutoGeral;
+import br.iteratorsystems.cps.entities.Tabelas_Usuario;
 import br.iteratorsystems.cps.exceptions.CpsGeneralExceptions;
+import br.iteratorsystems.cps.helper.ListaProdutoTOHelper;
+import br.iteratorsystems.cps.service.ListaProdutoService;
 import br.iteratorsystems.cps.to.ProdutoTO;
 
 public class testeListaDeProduto {
 
 	public static void main(String[] args) throws CpsGeneralExceptions {
 		
-		Set<ProdutoTO> listaProdutoTO = new HashSet<ProdutoTO>();
+		/*Set<ProdutoTO> listaProdutoTO = new HashSet<ProdutoTO>();
 		ProdutoTO produtoTO = new ProdutoTO(popular(),12);
 		listaProdutoTO.add(produtoTO); 
 		ListaDeProdutoBean bean = new ListaDeProdutoBean();
 		
-		//bean.setListaComprasUsuario(listaProdutoTO);
-		bean.incluirListaDeProdutos();
+		bean.setListaComprasUsuario(listaProdutoTO);
+		bean.incluirListaDeProdutos();*/
+		
+		Session session = HibernateConfig.getSession();
+		ListaProdutoDao listaProdutoDao = new ListaProdutoDao(Tabelas_ListaProduto.class, session);
+		Tabelas_ListaProduto listaProduto = listaProdutoDao.obter(1);
+		
+		ListaProdutoService listaProdutoService = new ListaProdutoService();
+		listaProdutoService.excluirListaDeProdutos(listaProduto);
+		
+		
+		/*ListaProdutoTOHelper helper = new ListaProdutoTOHelper(); 
+		tabelas'
+		Tabelas_Usuario usuario = ListaProdutoTOHelper.popularUsuario(null);
+		*/
 	}
 	
 	private static Tabelas_ProdutoGeral popular() {
 		Tabelas_ProdutoGeral prod  = new Tabelas_ProdutoGeral();
 		Set<Tabelas_ListaProdutoItem> lista = new HashSet<Tabelas_ListaProdutoItem>();
-		prod.setCodigoBarras("101010101");
+		prod.setCodigoBarras("789100000943");
 		prod.setDescricao("teste");
 		prod.setEmbalagem("sei la");
 		prod.setImagem('N');
