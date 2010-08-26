87d05e308276eead7bbbdc258f70c255eb09e96e
fabio.m.b.afonso <fabio.m.b.afonso@61f7c19e-d3f1-6563-da10-9b4bf67539cb>
2010-08-26 03:36:26 +0000

    cagada

---
+++ cps/src/br/iteratorsystems/cps/to/lojaTO.java
@@ -0,0 +1,222 @@
+package br.iteratorsystems.cps.to;
+
+import javax.faces.model.SelectItem;
+
+/**
+ * Classe Responsável por gerenciar a página default do sistema.
+ * @author Fabio
+ *
+ */
+
+public class lojaTO {
+	
+	private String razaosocial = null;
+	private String nomefantasia = null;
+	private String cnpj = null;
+	private String inscricaoestadual = null;
+	private String inscricaomunicipal = null;
+	private String nomedoresponsavelpelaloja = null;
+	private String tipoVendaSelecionada = null;
+	private SelectItem[] tipoVendas = null;
+	private String cep = null;
+	private String logradouro = null;
+	private String numero = null;
+	private String complemento = null;
+	private String bairro = null;
+	private String cidade = null;
+	private String estado = null;
+	private String telCom = null;
+	private String dddFax = null;
+	private String telFax = null;
+	private String dddCelCom = null;
+	private String telCelCom = null;
+	private String email = null;
+	private String homepage = null;
+	
+	/**
+	 * Construtor padrão.
+	 */
+	public lojaTO() {
+		
+	}
+
+	public String getRazaosocial() {
+		return razaosocial;
+	}
+
+	public void setRazaosocial(String razaosocial) {
+		this.razaosocial = razaosocial;
+	}
+
+	public String getNomefantasia() {
+		return nomefantasia;
+	}
+
+	public void setNomefantasia(String nomefantasia) {
+		this.nomefantasia = nomefantasia;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getInscricaoestadual() {
+		return inscricaoestadual;
+	}
+
+	public void setInscricaoestadual(String inscricaoestadual) {
+		this.inscricaoestadual = inscricaoestadual;
+	}
+
+	public String getInscricaomunicipal() {
+		return inscricaomunicipal;
+	}
+
+	public void setInscricaomunicipal(String inscricaomunicipal) {
+		this.inscricaomunicipal = inscricaomunicipal;
+	}
+
+	public String getNomedoresponsavelpelaloja() {
+		return nomedoresponsavelpelaloja;
+	}
+
+	public void setNomedoresponsavelpelaloja(String nomedoresponsavelpelaloja) {
+		this.nomedoresponsavelpelaloja = nomedoresponsavelpelaloja;
+	}
+
+	public String getTipoVendaSelecionada() {
+		return tipoVendaSelecionada;
+	}
+
+	public void setTipoVendaSelecionada(String tipoVendaSelecionada) {
+		this.tipoVendaSelecionada = tipoVendaSelecionada;
+	}
+
+	public SelectItem[] getTipoVendas() {
+		return tipoVendas;
+	}
+
+	public void setTipoVendas(SelectItem[] tipoVendas) {
+		this.tipoVendas = tipoVendas;
+	}
+
+	public String getCep() {
+		return cep;
+	}
+
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
+
+	public String getLogradouro() {
+		return logradouro;
+	}
+
+	public void setLogradouro(String logradouro) {
+		this.logradouro = logradouro;
+	}
+
+	public String getNumero() {
+		return numero;
+	}
+
+	public void setNumero(String numero) {
+		this.numero = numero;
+	}
+
+	public String getComplemento() {
+		return complemento;
+	}
+
+	public void setComplemento(String complemento) {
+		this.complemento = complemento;
+	}
+
+	public String getBairro() {
+		return bairro;
+	}
+
+	public void setBairro(String bairro) {
+		this.bairro = bairro;
+	}
+
+	public String getCidade() {
+		return cidade;
+	}
+
+	public void setCidade(String cidade) {
+		this.cidade = cidade;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getTelCom() {
+		return telCom;
+	}
+
+	public void setTelCom(String telCom) {
+		this.telCom = telCom;
+	}
+
+	public String getDddFax() {
+		return dddFax;
+	}
+
+	public void setDddFax(String dddFax) {
+		this.dddFax = dddFax;
+	}
+
+	public String getTelFax() {
+		return telFax;
+	}
+
+	public void setTelFax(String telFax) {
+		this.telFax = telFax;
+	}
+
+	public String getDddCelCom() {
+		return dddCelCom;
+	}
+
+	public void setDddCelCom(String dddCelCom) {
+		this.dddCelCom = dddCelCom;
+	}
+
+	public String getTelCelCom() {
+		return telCelCom;
+	}
+
+	public void setTelCelCom(String telCelCom) {
+		this.telCelCom = telCelCom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getHomepage() {
+		return homepage;
+	}
+
+	public void setHomepage(String homepage) {
+		this.homepage = homepage;
+	}
+	
+	
+	
+
+}
