7840ab890d7f0e753621269237377e9465b5189f
vagner.funny <vagner.funny@61f7c19e-d3f1-6563-da10-9b4bf67539cb>
2010-09-03 04:00:24 +0000

    Criação da Tabela para utilização dos dados de Latitude e Logintude, Criação dos campos novos para a tabela de parametrização. Lembrem de rodar o script do banco de dados e mantenham a Versão do banco atualizada. Segue uma cópia do banco de dados atualizada.

--- cps/src/br/iteratorsystems/cps/entities/Parametrizacao.java
+++ cps/src/br/iteratorsystems/cps/entities/Parametrizacao.java
@@ -17,18 +17,23 @@ public class Parametrizacao implements java.io.Serializable, EntityAble {
 	private String numMaxLojasComparacao;
 	private String diretorioProcessamentoXml;
 	private String diretorioImagensProCps;
+	private Double valormediocombustivel;
+    private Double rendimentomediocombustivel;
+
 
 	public Parametrizacao() {
 	}
 
 	public Parametrizacao(Integer idParametrizacao,
 			String numMaxItensLista, String numMaxLojasComparacao,
-			String diretorioProcessamentoXml, String diretorioImagensProCps) {
+			String diretorioProcessamentoXml, String diretorioImagensProCps, Double valormediocombustivel, Double rendimentomediocombustivel) {
 		this.idParametrizacao = idParametrizacao;
 		this.numMaxItensLista = numMaxItensLista;
 		this.numMaxLojasComparacao = numMaxLojasComparacao;
 		this.diretorioProcessamentoXml = diretorioProcessamentoXml;
 		this.diretorioImagensProCps = diretorioImagensProCps;
+		this.valormediocombustivel = valormediocombustivel;
+	    this.rendimentomediocombustivel = rendimentomediocombustivel;
 	}
 
 	@Id
@@ -76,16 +81,42 @@ public class Parametrizacao implements java.io.Serializable, EntityAble {
 	public void setDiretorioImagensProCps(String diretorioImagensProCps) {
 		this.diretorioImagensProCps = diretorioImagensProCps;
 	}
+	
+	@Column(name="valormediocombustivel", precision=17, scale=17)
+    public Double getValormediocombustivel() {
+        return this.valormediocombustivel;
+    }
+    
+    public void setValormediocombustivel(Double valormediocombustivel) {
+        this.valormediocombustivel = valormediocombustivel;
+    }
+    
+    @Column(name="rendimentomediocombustivel", precision=17, scale=17)
+    public Double getRendimentomediocombustivel() {
+        return this.rendimentomediocombustivel;
+    }
+    
+    public void setRendimentomediocombustivel(Double rendimentomediocombustivel) {
+        this.rendimentomediocombustivel = rendimentomediocombustivel;
+    }
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#toString()
+	 */
 	@Override
 	public String toString() {
 		return "Parametrizacao [diretorioImagensProCps="
 				+ diretorioImagensProCps + ", diretorioProcessamentoXml="
 				+ diretorioProcessamentoXml + ", idParametrizacao="
 				+ idParametrizacao + ", numMaxItensLista=" + numMaxItensLista
-				+ ", numMaxLojasComparacao=" + numMaxLojasComparacao + "]";
+				+ ", numMaxLojasComparacao=" + numMaxLojasComparacao
+				+ ", rendimentomediocombustivel=" + rendimentomediocombustivel
+				+ ", valormediocombustivel=" + valormediocombustivel + "]";
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#hashCode()
+	 */
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -108,16 +139,27 @@ public class Parametrizacao implements java.io.Serializable, EntityAble {
 				* result
 				+ ((numMaxLojasComparacao == null) ? 0 : numMaxLojasComparacao
 						.hashCode());
+		result = prime
+				* result
+				+ ((rendimentomediocombustivel == null) ? 0
+						: rendimentomediocombustivel.hashCode());
+		result = prime
+				* result
+				+ ((valormediocombustivel == null) ? 0 : valormediocombustivel
+						.hashCode());
 		return result;
 	}
 
+	/* (non-Javadoc)
+	 * @see java.lang.Object#equals(java.lang.Object)
+	 */
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
 			return true;
 		if (obj == null)
 			return false;
-		if (getClass() != obj.getClass())
+		if (!(obj instanceof Parametrizacao))
 			return false;
 		Parametrizacao other = (Parametrizacao) obj;
 		if (diretorioImagensProCps == null) {
@@ -146,6 +188,18 @@ public class Parametrizacao implements java.io.Serializable, EntityAble {
 				return false;
 		} else if (!numMaxLojasComparacao.equals(other.numMaxLojasComparacao))
 			return false;
+		if (rendimentomediocombustivel == null) {
+			if (other.rendimentomediocombustivel != null)
+				return false;
+		} else if (!rendimentomediocombustivel
+				.equals(other.rendimentomediocombustivel))
+			return false;
+		if (valormediocombustivel == null) {
+			if (other.valormediocombustivel != null)
+				return false;
+		} else if (!valormediocombustivel.equals(other.valormediocombustivel))
+			return false;
 		return true;
-	}	
+	}
+    
 }
